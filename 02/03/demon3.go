d8ed2ebd767a04bc9ee509af2c60233fed6fe955
yann <tmp4321@qq.com>
2019-12-19 19:53:59 +0800

    flag.CommandLine
    Signed-off-by: yann <tmp4321@qq.com>

--- 02/03/demon3.go
+++ 02/03/demon3.go
@@ -12,16 +12,17 @@ var name string
 //var name = flag.String("name", "everyone", "The greeting object.")
 
 func init() {
-	// 需在此处添加代码。[2]
+	flag.CommandLine = flag.NewFlagSet("", flag.ExitOnError)
+	flag.CommandLine.Usage = func() {
+		fmt.Fprintf(os.Stderr,"Usage of %s:\n","yann")
+		flag.PrintDefaults()
+	}
 	flag.StringVar(&name,"name","everyyone","The greeting object.")
 	//flag.String()
 }
 
 func main() {
-	flag.Usage = func(){
-		fmt.Fprintf(os.Stderr,"Usage of %s:\n","question")
-		flag.PrintDefaults()
-	}
+
 	flag.Parse()
 	fmt.Printf("Hello, %s!\n", name)
 }
