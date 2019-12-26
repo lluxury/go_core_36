b17dd8f03fe89aafa08e2b1a51ad91a1458196d2
yann <tmp4321@qq.com>
2019-12-26 18:41:52 +0800

    reflect
    Signed-off-by: yann <tmp4321@qq.com>

--- 14/01/demon32.go
+++ 14/01/demon32.go
@@ -24,14 +24,13 @@ func (dog Dog) Category() string {
 }
 
 func main()  {
-	dog1 := Dog{"little pig"}
-	fmt.Printf("The name of first dog is %q.\n", dog1.Name())
-	dog2 := dog1
-	fmt.Printf("The name of second dog is %q.\n", dog2.Name())
-	dog1.name = "monster"
-	fmt.Printf("The name of first dog is %q.\n", dog1.Name())
-	fmt.Printf("The name of second dog is %q.\n", dog2.Name())
-	fmt.Println()
+	dog := Dog{"little pig"}
+	fmt.Printf("The dog's name is %q.\n", dog.Name())
+	pet := &dog
+	dog.SetName("monster")
+	fmt.Printf("The dog's name is %q.\n", dog.Name())
+	fmt.Printf("This pet is a %s, the name is %q.\n",
+	pet.Category(), pet.Name())
 
 
 }
