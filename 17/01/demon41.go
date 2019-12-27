36c146bec77ead0c87aadb600d3295fd2155e05a
yann <tmp4321@qq.com>
2019-12-27 21:53:28 +0800

    for range 3
    Signed-off-by: yann <tmp4321@qq.com>

--- 17/01/demon41.go
+++ 17/01/demon41.go
@@ -3,16 +3,15 @@ package main
 import "fmt"
 
 func main(){
-	numbers2 := [...]int{1, 2, 3, 4, 5, 6}
-	//maxIndex2 := len(numbers2)
-	maxIndex2 := len(numbers2) -1
-	for i, e := range numbers2 {
-		if i == maxIndex2 {
-		numbers2[0] += e
+	numbers2 := []int{1, 2, 3, 4, 5, 6}
+	numbers3 := [...]int{1, 2, 3, 4, 5, 6}
+	maxIndex3 := len(numbers2) -1
+	for i, e := range numbers3 {
+		if i == maxIndex3 {
+		numbers3[0] += e
 		} else {
-			numbers2[i+1] += e
+			numbers3[i+1] += e
 		}
 	}
-	fmt.Println(numbers2)
-	fmt.Println()
+	fmt.Println(numbers3)
 	}
