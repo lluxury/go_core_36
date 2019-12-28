b9a7a6ff08de2305eac29daae608655a10f4253d
yann <tmp4321@qq.com>
2019-12-28 11:14:25 +0800

    error
    Signed-off-by: yann <tmp4321@qq.com>

--- 18/02/demon46.go
+++ 18/02/demon46.go
@@ -29,12 +29,38 @@ func (e Errno) Error() string {
 
 func main()  {
 	var err error
-	_, err = exec.LookPath(os.DevNull)
-	fmt.Printf("error: %s\n", err)
-	if execErr, ok := err.(*exec.Error); ok {
-		execErr.Name = os.TempDir()
-		execErr.Err = os.ErrNotExist
+	//	//_, err = exec.LookPath(os.DevNull)
+	//	//fmt.Printf("error: %s\n", err)
+	//	//if execErr, ok := err.(*exec.Error); ok {
+	//	//	execErr.Name = os.TempDir()
+	//	//	execErr.Err = os.ErrNotExist
+	//	//}
+	//	//fmt.Printf("error: %s\n", err)
+	//	//fmt.Println()
+
+	err = os.ErrPermission
+	if os.IsPermission(err){
+		fmt.Printf("error(permission): %s\n", err)
+	} else {
+		fmt.Printf("error(other): %s\n", err)
 	}
-	fmt.Printf("error: %s\n", err)
+	os.ErrPermission = os.ErrExist
+	//错误演示？
+	// 上面这行代码修改了os包中已定义的错误值。
+	// 这样做会导致下面判断的结果不正确。
+	// 并且，这会影响到当前Go程序中所有的此类判断。
+	// 所以，一定要避免这样做！
+	if os.IsPermission(err){
+		fmt.Printf("error(permission:) %s\n",err)
+	} else {
+		fmt.Printf("error(other): %s\n", err)
+	}
+
 	fmt.Println()
 }
+
+//error(permission): permission denied
+//error(permission): permission denied
+// //结果对比
+//error(permission): permission denied
+//error(other): permission denied
