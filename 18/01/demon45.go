e75d9112d106874ed37983065a6b98da282a01ef
yann <tmp4321@qq.com>
2019-12-28 09:53:16 +0800

    os.IsExist()
    Signed-off-by: yann <tmp4321@qq.com>

--- 18/01/demon45.go
+++ 18/01/demon45.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"runtime"
 )
 
 func underlyingError(err error) error {
@@ -22,53 +23,44 @@ func underlyingError(err error) error {
 func main(){
 	_, _, err := os.Pipe()
 
-	paths := []string{
-		os.Args[0], // 当前文件
+	paths2 := []string{
+		runtime.GOROOT(), // Go根目录
 		"/it/must/not/exist", // 肯定不存在的目录。
 		os.DevNull,  // 存在的目录
 	}
-	printError := func(i int, err error) {
+	printError2 := func(i int, err error) {
 		if err != nil {
 			fmt.Println("nil errors")
 			return
 		}
 		err = underlyingError(err)
-		switch err {
-		case os.ErrClosed:
+		if os.IsExist(err) {
 			fmt.Printf("error(closed)[%d]: %s\n", i, err)
-		case os.ErrInvalid:
+		} else if os.IsNotExist(err){
 			fmt.Printf("error(invalid)[%d]: %s\n", i, err)
-		case os.ErrPermission:
+		} else if os.IsPermission(err) {
 			fmt.Printf("error(permission)[%d]: %s\n", i, err)
+		} else {
+			fmt.Printf("error(other)[%d]: %s\n", i, err)
 		}
 	}
 	var f *os.File
 	var index int
 	{
 		index = 0
-		f, err := os.Open(paths[index])
-		if err != nil {
-			fmt.Printf("unexpected error: %s\n", err)
-			return
-		}
-		//制造错误为 os.ErrClosed
-		f.Close()
-		_, err = f.Read([]byte{})
-		printError(index,err)
+		err = os.Mkdir(paths2[index], 0700)
+		printError2(index,err)
 	}
 
 	{
 		index = 1
-		//制造 os.ErrInvalid 错误
-		f, _ = os.Open(paths[index])
-		_, err = f.Stat()
-		printError(index,err)
+		f, err = os.Open(paths2[index])
+		printError2(index,err)
 	}
 	{
 		index = 2
-		//制造 os.ErrPermission 的错误
-		_, err = exec.LookPath(paths[index])
-		printError(index,err)
+		_, err = exec.LookPath(paths2[index])
+		printError2(index,err)
 	}
 	if f != nil {
 		f.Close()
