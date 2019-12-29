386b6de4a358cb3ed00bf0b00a6fcb81d768c4a2
yann <tmp4321@qq.com>
2019-12-29 23:56:19 +0800

    testing
    Signed-off-by: yann <tmp4321@qq.com>

---
+++ 20/03/demon54_test.go
@@ -0,0 +1,11 @@
+package s3
+
+import (
+	"testing"
+)
+
+func BenchmarkGetPrimes(b *testing.B){
+	for i := 0; i < b.N; i++ {
+	  GetPrimes(1000)
+	}
+}
---
+++ 21/03/demon57_test.go
@@ -0,0 +1,16 @@
+package q3
+
+import (
+	"testing"
+	"time"
+)
+
+func BenchmarkGetPrimes(b *testing.B){
+	b.StopTimer()
+	time.Sleep(time.Millisecond * 500)  // 模拟耗时操作
+	max := 10000
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+	  GetPrimes(max)
+	}
+}
