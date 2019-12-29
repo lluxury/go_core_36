0cd2e6955c3d6156e398054b893c1ed5efa91135
yann <tmp4321@qq.com>
2019-12-29 15:49:06 +0800

    first test
    Signed-off-by: yann <tmp4321@qq.com>

---
+++ 20/00/demon52.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+)
+
+var name string
+
+func init() {
+	flag.StringVar(&name, "name", "everyone", "The greeting object.")
+}
+
+func main() {
+	flag.Parse()
+	greeting, err := hello(name)
+	if err != nil {
+		fmt.Printf("error: %s\n", err)
+		return
+	}
+	fmt.Println(greeting, introduce())
+}
+
+func hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	return fmt.Sprintf("Hello, %s!", name), nil
+	//S 只带一个值，Printf 带2个
+}
+
+func introduce() string {
+	return "Welcome to my Golang column."
+}
---
+++ 20/00/demon52_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	var name string
+	greeting, err := hello(name)
+	//if err != nil {
+	if err == nil {		// 无错,无值检测
+		t.Errorf("The error is nil, but it should not be. (name=%q)",
+			name)
+	}
+
+	if greeting != "" {   // 空
+		t.Errorf("Nonempty greeting, but it should not be. (name=%q)",
+			name)
+	}
+
+	name = "Robert"
+	greeting, err = hello(name)
+	if err != nil {    // 有错
+		t.Errorf("The error is not nil, but it should be. (name=%q)",
+			name)
+	}
+
+	//if greeting != "" {
+	if greeting == "" {   // 无值
+		t.Errorf("Empty greeting, but it should not be. (name=%q)",
+			name)
+	}
+	expected := fmt.Sprintf("Hello, %s!", name)
+	if greeting != expected {    // 非期望值
+		t.Errorf("The actual greeting %q is not the expected. (name=%q)",
+			greeting, name)
+	}
+	t.Logf("The expected greeting is %q.\n", expected)
+}
+
+func testIntroduce(t *testing.T) {
+	intro := introduce()
+	expected := "Welcome to my Golang column."  // 匹配值
+	if intro != expected {
+		t.Errorf("The actual introduce %q is not the expected.",
+			intro)
+	}
+	t.Logf("The expected introduce is %q.\n", expected)
+}
