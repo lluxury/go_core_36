c3c71cc69d122c00a02706c007bcf9dcb6fe1d11
yann <tmp4321@qq.com>
2019-12-31 17:09:19 +0800

    atomic.Value string
    Signed-off-by: yann <tmp4321@qq.com>

--- 24/02/demon64.go
+++ 24/02/demon64.go
@@ -7,15 +7,21 @@ import (
 
 func main() {
 	var box atomic.Value
-	fmt.Println("Copy box to box2.",)
+	//fmt.Println("Copy box to box2.",)
 	box2 := box
 	v1 := [...]int{1, 2, 3}
-	fmt.Printf("Store %v to box.\n", v1)
+	//fmt.Printf("Store %v to box.\n", v1)
 	box.Store(v1)
-	fmt.Printf("The value load from box is %v.\n", box.Load())
-	fmt.Printf("The value load from box2 is %v.\n", box2.Load())
-	fmt.Println("",)
+	//fmt.Printf("The value load from box is %v.\n", box.Load())
+	//fmt.Printf("The value load from box2 is %v.\n", box2.Load())
+	//fmt.Println("",)
 
+	v2 := "123"
+	fmt.Printf("Store %q to box2.\n",v2)
+	box2.Store(v2)
+	fmt.Printf("The value load from box is %v.\n", box.Load())
+	fmt.Printf("The value load from box2 is %q.\n", box2.Load())
+	fmt.Println()
 
 
 
