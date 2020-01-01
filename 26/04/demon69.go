984beaa798bb9e085ab8e51f2fbd10dfba548f80
yann <tmp4321@qq.com>
2020-01-01 14:11:03 +0800

    context.WithValue branch
    Signed-off-by: yann <tmp4321@qq.com>

--- 26/04/demon69.go
+++ 26/04/demon69.go
@@ -55,12 +55,21 @@ func main()  {
 	//fmt.Println()
 
 	node6Branch := context.WithValue(node5, keys[3],values[3])
-	fmt.Printf("The value of the key %v found in the node6Branch: %v\n",
-		keys[1], node6Branch.Value(keys[1]))
-	fmt.Printf("The value of the key %v found in the node6Branch: %v\n",
-		keys[2], node6Branch.Value(keys[2]))
-	fmt.Printf("The value of the key %v found in the node6Branch: %v\n",
-		keys[3], node6Branch.Value(keys[3]))
-	fmt.Println()
+	_ = node6Branch
+	//fmt.Printf("The value of the key %v found in the node6Branch: %v\n",
+	//	keys[1], node6Branch.Value(keys[1]))
+	//fmt.Printf("The value of the key %v found in the node6Branch: %v\n",
+	//	keys[2], node6Branch.Value(keys[2]))
+	//fmt.Printf("The value of the key %v found in the node6Branch: %v\n",
+	//	keys[3], node6Branch.Value(keys[3]))
+	//fmt.Println()
 
+	node7, _ := context.WithCancel(node6)
+	node8, _ := context.WithTimeout(node7, time.Hour)
+	fmt.Printf("The value of the key %v found in the node8: %v\n",
+		keys[1], node8.Value(keys[1]))
+	fmt.Printf("The value of the key %v found in the node8: %v\n",
+		keys[2], node8.Value(keys[2]))
+	fmt.Printf("The value of the key %v found in the node8: %v\n",
+		keys[3], node8.Value(keys[3]))
 }
