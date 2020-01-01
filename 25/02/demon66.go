26765cc11f500522d0f5a9fa8bb163eb88fa45fe
yann <tmp4321@qq.com>
2020-01-01 11:12:26 +0800

    sync.Once and sync.WaitGroup
    Signed-off-by: yann <tmp4321@qq.com>

--- 25/02/demon66.go
+++ 25/02/demon66.go
@@ -3,21 +3,52 @@ package main
 import (
 	"fmt"
 	"sync"
-	"sync/atomic"
+	//"sync/atomic"
+	"time"
 )
 
 func main() {
-	var counter uint32
-	var once sync.Once
-	once.Do(func() {
-		atomic.AddUint32(&counter, 1)
-	})
-	fmt.Printf("The counter: %d\n",counter)
-	once.Do(func() {
-		atomic.AddUint32(&counter,2)
-	})
-	fmt.Printf("The counter: %d\n", counter)
+	//var counter uint32
+	//var once sync.Once
+	//once.Do(func() {
+	//	atomic.AddUint32(&counter, 1)
+	//})
+	//fmt.Printf("The counter: %d\n",counter)
+	//once.Do(func() {
+	//	atomic.AddUint32(&counter,2)
+	//})
+	//fmt.Printf("The counter: %d\n", counter)
+	//fmt.Println("",)
+	
+	once := sync.Once{}
+	var wg sync.WaitGroup
+	wg.Add(3)
+	go func() {
+		defer wg.Done()
+		once.Do(func() {
+			for i := 0; i < 3; i++ {
+				fmt.Printf("Do task. [1-%d]\n",	i)
+				time.Sleep(time.Second)
+			}
+		})
+		fmt.Println("Done. [1]",)
+	}()
+	go func() {
+		defer wg.Done()
+		time.Sleep(time.Millisecond * 500)
+		once.Do(func() {
+			fmt.Println("Do task. [2]",)
+		})
+		fmt.Println("Done. [2]",)
+	}()
+	go func() {
+		defer wg.Done()
+		time.Sleep(time.Millisecond * 500)
+		once.Do(func() {
+			fmt.Println("Do task, [3]",)
+		})
+		fmt.Println("Done. [3]",)
+	}()
+	wg.Wait()
 	fmt.Println("",)
-	
-	
 }
