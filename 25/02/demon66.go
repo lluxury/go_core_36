5cdf43733cc51bda8406fd4d580acc390fa68f1c
yann <tmp4321@qq.com>
2020-01-01 11:27:27 +0800

    sync.Once panic
    Signed-off-by: yann <tmp4321@qq.com>

--- 25/02/demon66.go
+++ 25/02/demon66.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"sync"
 	//"sync/atomic"
@@ -20,35 +21,60 @@ func main() {
 	//fmt.Printf("The counter: %d\n", counter)
 	//fmt.Println("",)
 	
-	once := sync.Once{}
+	//once := sync.Once{}
 	var wg sync.WaitGroup
-	wg.Add(3)
+	//wg.Add(3)
+	//go func() {
+	//	defer wg.Done()
+	//	once.Do(func() {
+	//		for i := 0; i < 3; i++ {
+	//			fmt.Printf("Do task. [1-%d]\n",	i)
+	//			time.Sleep(time.Second)
+	//		}
+	//	})
+	//	fmt.Println("Done. [1]",)
+	//}()
+	//go func() {
+	//	defer wg.Done()
+	//	time.Sleep(time.Millisecond * 500)
+	//	once.Do(func() {
+	//		fmt.Println("Do task. [2]",)
+	//	})
+	//	fmt.Println("Done. [2]",)
+	//}()
+	//go func() {
+	//	defer wg.Done()
+	//	time.Sleep(time.Millisecond * 500)
+	//	once.Do(func() {
+	//		fmt.Println("Do task, [3]",)
+	//	})
+	//	fmt.Println("Done. [3]",)
+	//}()
+	//wg.Wait()
+	//fmt.Println("",)
+
+	once := sync.Once{}
+	wg.Add(2)
 	go func() {
 		defer wg.Done()
-		once.Do(func() {
-			for i := 0; i < 3; i++ {
-				fmt.Printf("Do task. [1-%d]\n",	i)
-				time.Sleep(time.Second)
+		defer func() {
+			if p := recover(); p != nil {
+				fmt.Printf("fatal error:%v\n",p)
 			}
+		}()
+		once.Do(func() {
+			fmt.Println("Do task. [4]",)
+			panic(errors.New("something wrong"))
+			fmt.Println("Done.[4]",)
 		})
-		fmt.Println("Done. [1]",)
 	}()
 	go func() {
 		defer wg.Done()
 		time.Sleep(time.Millisecond * 500)
 		once.Do(func() {
-			fmt.Println("Do task. [2]",)
+			fmt.Println("Do task. [5]",)  // 没有打出来，once 失效
 		})
-		fmt.Println("Done. [2]",)
-	}()
-	go func() {
-		defer wg.Done()
-		time.Sleep(time.Millisecond * 500)
-		once.Do(func() {
-			fmt.Println("Do task, [3]",)
-		})
-		fmt.Println("Done. [3]",)
+		fmt.Println("Done. [5]",)
 	}()
 	wg.Wait()
-	fmt.Println("",)
 }
