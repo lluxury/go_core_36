120cf03decf383ba63872ffdb5b75c89ac75a79b
yann <tmp4321@qq.com>
2020-01-03 21:04:03 +0800

    reader1.ReadAt()
    Signed-off-by: yann <tmp4321@qq.com>

--- 30/03/demon77.go
+++ 30/03/demon77.go
@@ -15,9 +15,18 @@ func main() {
 	buf1 := make([]byte, 47)
 	n, _ := reader1.Read(buf1)
 	fmt.Printf("%d bytes were read. (callRead)\n",n)
-	fmt.Printf("The reading index in reader: %d]n",
+	fmt.Printf("The reading index in reader: %d]\n",
 		reader1.Size()-int64(reader1.Len()))
 	fmt.Println("",)
+
+	buf2 := make([]byte, 21)
+	offset1 := int64(64)
+	n, _ = reader1.ReadAt(buf2, offset1)
+	fmt.Printf("%d bytes were read. (call ReadAt, offset: %d)\n",n, offset1)
+	fmt.Printf("The reading index in reader: %d\n",
+		reader1.Size()-int64(reader1.Len()))
+	fmt.Println("",)
+	
 	
 	
 	
