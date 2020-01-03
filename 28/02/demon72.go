c5acf7645a7cfd0fb5ca9d7eff580b887d1a2b5c
yann <tmp4321@qq.com>
2020-01-03 13:42:50 +0800

    ConcurrentMap
    Signed-off-by: yann <tmp4321@qq.com>

--- 28/02/demon72.go
+++ 28/02/demon72.go
@@ -1,46 +1,109 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"reflect"
 	"sync"
 )
 
-type IntStrMap struct {
-	m sync.Map
+//type IntStrMap struct {
+//	m sync.Map
+//}
+//
+//func (iMap *IntStrMap) Delete(key int)  {
+//	iMap.m.Delete(key)
+//}
+//
+//func (iMap *IntStrMap) Load(key int) (value string, ok bool) {
+//	//v, ok := iMap.Load(key)
+//	v, ok := iMap.m.Load(key)
+//	if v != nil {
+//		value = v.(string)
+//	}
+//	return
+//}
+//
+//func (iMap *IntStrMap) LoadOrStore(key int, value string) (actual string, loaded bool) {
+//	a,loaded := iMap.m.LoadOrStore(key, value)
+//	actual = a.(string)
+//	return
+//}
+//
+//func (iMap *IntStrMap) Range(f func(key int, value string) bool) {
+//	f1 := func(key,value interface{}) bool {
+//		return f(key.(int), value.(string))
+//	}
+//	iMap.m.Range(f1)
+//}
+//
+//func (iMap *IntStrMap) Store(key int, value string)  {
+//	iMap.m.Store(key, value)
+//}
+
+
+type ConcurrentMap struct {
+	m 			sync.Map
+	keyType 	reflect.Type
+	valueType 	reflect.Type
 }
 
-func (iMap *IntStrMap) Delete(key int)  {
-	iMap.m.Delete(key)
-}
-
-func (iMap *IntStrMap) Load(key int) (value string, ok bool) {
-	//v, ok := iMap.Load(key)
-	v, ok := iMap.m.Load(key)
-	if v != nil {
-		value = v.(string)
+func NewConcurrentMap(keyType, valueType reflect.Type) (*ConcurrentMap, error) {
+	if keyType == nil {
+		return nil, errors.New("nil key type")
 	}
+	if !keyType.Comparable(){
+		return nil, fmt.Errorf("incomparable key type: %s", keyType)
+	}
+	if !valueType.Comparable(){
+		return nil, errors.New("nil value type")
+	}
+	cMap := &ConcurrentMap{
+		keyType:   keyType,
+		valueType: valueType,
+	}
+	return cMap, nil
+}
+
+func (cMap *ConcurrentMap) Delete(key interface{}) {
+	if reflect.TypeOf(key) != cMap.keyType{
+		return
+	}
+	cMap.m.Delete(key)
+}
+
+func (cMap *ConcurrentMap) Load(key interface{}) (value interface{}, ok bool){
+	if reflect.TypeOf(key) != cMap.keyType{
+		return
+	}
+	return cMap.m.Load(key)
+}
+
+func (cMap *ConcurrentMap) LoadOrStore(key, value interface{})(actual interface{},loaded bool) {
+	if reflect.TypeOf(key) != cMap.keyType{
+		panic(fmt.Errorf("Wrong key type: %v", reflect.TypeOf(key)))
+	}
+	if reflect.TypeOf(value) != cMap.valueType {
+		panic(fmt.Errorf("wrong value type: %v", reflect.TypeOf(value)))
+	}
+	//actual,loaded = cMap.m.Load(key,value)
+	actual, loaded = cMap.m.LoadOrStore(key, value)
 	return
 }
 
-func (iMap *IntStrMap) LoadOrStore(key int, value string) (actual string, loaded bool) {
-	a,loaded := iMap.m.LoadOrStore(key, value)
-	actual = a.(string)
-	return
+func (cMap *ConcurrentMap) Range(f func(key, value interface{}) bool)  {
+	cMap.m.Range(f)
 }
 
-func (iMap *IntStrMap) Range(f func(key int, value string) bool) {
-	f1 := func(key,value interface{}) bool {
-		return f(key.(int), value.(string))
+func (cMap *ConcurrentMap) Store(key, value interface{}) {
+	if reflect.TypeOf(key) != cMap.keyType{
+		panic(fmt.Errorf("wrong key type: %v", reflect.TypeOf(key)))
 	}
-	iMap.m.Range(f1)
+	if reflect.TypeOf(value) != cMap.valueType{
+		panic(fmt.Errorf("wrong value type: %v", reflect.TypeOf(value)))
+	}
+	cMap.m.Store(key,value)
 }
-
-func (iMap *IntStrMap) Store(key int, value string)  {
-	iMap.m.Store(key, value)
-}
-
-
-
 
 
 var pairs = []struct{
@@ -60,53 +123,112 @@ func main() {
 	//sMap.Store([]int{1, 2, 3},4)
 	//_ = sMap
 
-	var iMap IntStrMap
-	iMap.Store(pairs[0].k, pairs[0].v)
-	iMap.Store(pairs[1].k, pairs[1].v)
-	iMap.Store(pairs[2].k, pairs[2].v)
-	fmt.Println("[Three pairs have been stored in the IntStrMap instance]")
-	
-	iMap.Range(func(key int, value string) bool {
-		fmt.Printf("The result of an iteration in Range: %d, %s\n",
-			key,value)
-		return true
-	})
+	//{
+	//var iMap IntStrMap
 
-	k0 := pairs[0].k
-	v0, ok := iMap.Load(k0)
-	fmt.Printf("The result of Load: %v, %v (key: %v)\n",
-		v0, ok, k0)
+	//iMap.Store(pairs[0].k, pairs[0].v)
+	//iMap.Store(pairs[1].k, pairs[1].v)
+	//iMap.Store(pairs[2].k, pairs[2].v)
+	//fmt.Println("[Three pairs have been stored in the IntStrMap instance]")
+	//
+	//iMap.Range(func(key int, value string) bool {
+	//	fmt.Printf("The result of an iteration in Range: %d, %s\n",
+	//		key,value)
+	//	return true
+	//})
+	//
+	//k0 := pairs[0].k
+	//v0, ok := iMap.Load(k0)
+	//fmt.Printf("The result of Load: %v, %v (key: %v)\n",
+	//	v0, ok, k0)
+	//
+	//k3 := pairs[3].k
+	//v3, ok := iMap.Load(k3)
+	//fmt.Printf("The result of Load: %v, %v (key: %v)\n",
+	//	v3, ok, k3)
+	//
+	//k2, v2 := pairs[2].k, pairs[2].v
+	//actual2, loaded2 := iMap.LoadOrStore(k2,v2)
+	//fmt.Printf("The result of LoadOrStore: %v, %v (key: %v, value: %v)\n",
+	//	actual2,loaded2,k2,v2)
+	//v3 = pairs[3].v
+	//actual3, loaded3 := iMap.LoadOrStore(k3,v3)
+	//fmt.Printf("The result of LoadOrStore: %v, %v (key: %v, value: %v)\n",
+	//	actual3, loaded3, k3, v3)
+	//
+	//k1 := pairs[1].k
+	//iMap.Delete(k1)
+	//fmt.Printf("[The pair with the key of %v has been removed from the IntStrMap instance]\n",
+	//	k1)
+	//v1,ok := iMap.Load(k1)
+	//fmt.Printf("The result of Load: %v, %v (key: %v)\n",
+	//	v1, ok, k1)
+	//v1 = pairs[1].v
+	//actual1, loaded1 := iMap.LoadOrStore(k1,v1)
+	//fmt.Printf("The result of LoadOrStore: %v, %v (key: %v, value: %v)\n",
+	//	actual1, loaded1, k1, v1)
+	//
+	//iMap.Range(func(key int, value string) bool {
+	//	fmt.Printf("The result of an iteration in Range: %d, %s\n",
+	//		key,value)
+	//	return true
+	//})
+	//fmt.Println("",)
+	//}
 
-	k3 := pairs[3].k
-	v3, ok := iMap.Load(k3)
-	fmt.Printf("The result of Load: %v, %v (key: %v)\n",
-		v3, ok, k3)
+	{
+		cMap, err := NewConcurrentMap(
+			reflect.TypeOf(pairs[0].k), reflect.TypeOf(pairs[0].v))
+		if err != nil {
+			fmt.Printf("fatal error: %s", err)
+			return
+		}
+		cMap.Store(pairs[0].k, pairs[0].v)
+		cMap.Store(pairs[1].k, pairs[1].v)
+		cMap.Store(pairs[2].k, pairs[2].v)
+		fmt.Println("[Three pairs have been stored in the ConcurrentMap instance]")
 
-	k2, v2 := pairs[2].k, pairs[2].v
-	actual2, loaded2 := iMap.LoadOrStore(k2,v2)
-	fmt.Printf("The result of LoadOrStore: %v, %v (key: %v, value: %v)\n",
-		actual2,loaded2,k2,v2)
-	v3 = pairs[3].v
-	actual3, loaded3 := iMap.LoadOrStore(k3,v3)
-	fmt.Printf("The result of LoadOrStore: %v, %v (key: %v, value: %v)\n",
-		actual3, loaded3, k3, v3)
+		cMap.Range(func(key, value interface{}) bool {
+			fmt.Printf("The result of an iteration in Range: %d, %s\n",
+				key, value)
+			return true
+		})
 
-	k1 := pairs[1].k
-	iMap.Delete(k1)
-	fmt.Printf("[The pair with the key of %v has been removed from the IntStrMap instance]\n",
-		k1)
-	v1,ok := iMap.Load(k1)
-	fmt.Printf("The result of Load: %v, %v (key: %v)\n",
-		v1, ok, k1)
-	v1 = pairs[1].v
-	actual1, loaded1 := iMap.LoadOrStore(k1,v1)
-	fmt.Printf("The result of LoadOrStore: %v, %v (key: %v, value: %v)\n",
-		actual1, loaded1, k1, v1)
-	
-	iMap.Range(func(key int, value string) bool {
-		fmt.Printf("The result of an iteration in Range: %d, %s\n",
-			key,value)
-		return true
-	})
-	fmt.Println("",)
+		k0 := pairs[0].k
+		v0, ok := cMap.Load(k0)
+		fmt.Printf("The result of Load: %v, %v (key: %v)\n",
+			v0, ok, k0)
+
+		k3 := pairs[3].k
+		v3, ok := cMap.Load(k3)
+		fmt.Printf("The result of Load: %v, %v (key: %v)\n",
+			v3, ok, k3)
+
+		k2, v2 := pairs[2].k, pairs[2].v
+		actual2, loaded2 := cMap.LoadOrStore(k2,v2)
+		fmt.Printf("The result of LoadOrStore: %v, %v (key: %v, value: %v)\n",
+			actual2, loaded2, k2, v2)
+		v3 = pairs[3].v
+		actual3, loaded3 := cMap.LoadOrStore(k3,v3)
+		fmt.Printf("The result of LoadOrStore: %v, %v (key: %v, value: %v)\n",
+			actual3, loaded3, k3, v3)
+
+		k1 := pairs[1].k
+		cMap.Delete(k1)
+		fmt.Printf("[The pair with the key of %v has been removed from the ConcurrentMap instance]\n",
+			k1)
+		v1, ok := cMap.Load(k1)
+		fmt.Printf("The result of Load: %v, %v (key: %v)\n",
+			v1, ok, k1)
+		v1 = pairs[1].v
+		actual1, loaded1 := cMap.LoadOrStore(k1,v1)
+		fmt.Printf("The result of LoadOrStore: %v, %v (key: %v, value: %v)\n",
+			actual1, loaded1, k1, v1)
+		
+		cMap.Range(func(key, value interface{}) bool {
+			fmt.Printf("The result of an iteration in Range: %d, %s\n",
+				key, value)
+			return true
+		})
+	}
 }
