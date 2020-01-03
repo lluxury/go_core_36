bb644252f98e87677fc43ff3065c9c51c499d5c9
yann <tmp4321@qq.com>
2020-01-03 18:55:00 +0800

    strings.NewReader()
    Signed-off-by: yann <tmp4321@qq.com>

---
+++ 30/03/demon77.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	reader1 := strings.NewReader("NewReader returns a new Reader reading from s. " +
+		"It is similar to bytes.NewBufferString but more efficient and read-only.")
+	fmt.Printf("The size of reader: %d\n", reader1.Size())
+	fmt.Printf("The reading index in reader: %d\n",
+		reader1.Size()-int64(reader1.Len()))
+	
+	buf1 := make([]byte, 47)
+	n, _ := reader1.Read(buf1)
+	fmt.Printf("%d bytes were read. (callRead)\n",n)
+	fmt.Printf("The reading index in reader: %d]n",
+		reader1.Size()-int64(reader1.Len()))
+	fmt.Println("",)
+	
+	
+	
+	
+
+}
