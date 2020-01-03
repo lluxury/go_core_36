0ca2b44482daf6794d5cdbf04083eb5c3e28231c
yann <tmp4321@qq.com>
2020-01-03 17:50:18 +0800

    builder1.Grow
    Signed-off-by: yann <tmp4321@qq.com>

--- 30/01/demon75.go
+++ 30/01/demon75.go
@@ -8,15 +8,20 @@ import (
 func main() {
 	var builder1 strings.Builder
 	builder1.WriteString("A Builder is used to efficiently build a string using Write methods.")
-	fmt.Printf("The first output(%d):\n%q\n", builder1.Len(),builder1.String())
-	fmt.Println("",)
-	//builder1.WriteByte('  ')
+	//fmt.Printf("The first output(%d):\n%q\n", builder1.Len(),builder1.String())
+	//fmt.Println("",)
 	builder1.WriteByte(' ')
 	builder1.WriteString("It minimizes memory copying. The zero value is ready to use.")
 	builder1.Write([]byte{'\n', '\n'})
 	builder1.WriteString("Do not copy a non-zero Builder.")
-	fmt.Printf("The second output(%d):\n\"%s\"\n", builder1.Len(), builder1.String())
+	//fmt.Printf("The second output(%d):\n\"%s\"\n", builder1.Len(), builder1.String())
+	//fmt.Println()
+
+	fmt.Println("Grow the builder ...")
+	builder1.Grow(10)
+	fmt.Printf("The length of contents in the builder is %d.\n", builder1.Len())
 	fmt.Println()
+
 
 
 
