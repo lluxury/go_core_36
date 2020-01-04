7e2494eebb1953b92dfccf9ac7e282b577b21323
yann <tmp4321@qq.com>
2020-01-05 00:31:55 +0800

    buffer Grow

--- 31/02/demon79.go
+++ 31/02/demon79.go
@@ -7,32 +7,44 @@ import (
 
 func main() {
 	var contents string
-	buffer1 := bytes.NewBufferString(contents)
-	fmt.Printf("The length of new buffer with contents %q: %d\n", 
-		contents,buffer1.Len())
-	fmt.Printf("The capacity of new buffer with contents %q: %d\n", 
-		contents,buffer1.Cap())	
-	fmt.Println("")
+	// buffer1 := bytes.NewBufferString(contents)
+	// fmt.Printf("The length of new buffer with contents %q: %d\n", 
+	// 	contents,buffer1.Len())
+	// fmt.Printf("The capacity of new buffer with contents %q: %d\n", 
+	// 	contents,buffer1.Cap())	
+	// fmt.Println("")
 
-	contents = "12345"
-	// fmt.Printf("Write contents $q ...\n", contents)
-	fmt.Printf("Write contents %q ...\n", contents)
-	buffer1.WriteString(contents)
-	fmt.Printf("The length of buffer: %d\n", buffer1.Len())
-	fmt.Printf("The capacity of buffer: %d\n", buffer1.Cap())
-	fmt.Println("")
+	// contents = "12345"
+	// // fmt.Printf("Write contents $q ...\n", contents)
+	// fmt.Printf("Write contents %q ...\n", contents)
+	// buffer1.WriteString(contents)
+	// fmt.Printf("The length of buffer: %d\n", buffer1.Len())
+	// fmt.Printf("The capacity of buffer: %d\n", buffer1.Cap())
+	// fmt.Println("")
 
-	contents = "67"
-	fmt.Printf("Write contents %q ...\n", contents)
-	buffer1.WriteString(contents)
-	fmt.Printf("The length of buffer: %d\n", buffer1.Len())
-	fmt.Printf("The capacity of buffer: %d\n", buffer1.Cap())
-	fmt.Println("")
+	// contents = "67"
+	// fmt.Printf("Write contents %q ...\n", contents)
+	// buffer1.WriteString(contents)
+	// fmt.Printf("The length of buffer: %d\n", buffer1.Len())
+	// fmt.Printf("The capacity of buffer: %d\n", buffer1.Cap())
+	// fmt.Println("")
 
-	contents = "89"
-	fmt.Printf("Write contents %q ...\n", contents)
-	buffer1.WriteString(contents)
-	fmt.Printf("The length of buffer: %d\n", buffer1.Len())
-	fmt.Printf("The capacity of buffer: %d\n", buffer1.Cap())
+	// contents = "89"
+	// fmt.Printf("Write contents %q ...\n", contents)
+	// buffer1.WriteString(contents)
+	// fmt.Printf("The length of buffer: %d\n", buffer1.Len())
+	// fmt.Printf("The capacity of buffer: %d\n", buffer1.Cap())
+	// fmt.Println("")
+
+	contents = "abcdefghijk"
+	buffer2 := bytes.NewBufferString(contents)
+	fmt.Printf("The length of buffer: %d\n", buffer2.Len())
+	fmt.Printf("The capacity of buffer: %d\n", buffer2.Cap())
 	fmt.Println("")
+	n := 10
+	fmt.Printf("Grow the bufer with %d ...\n", n)
+	buffer2.Grow(n)
+	fmt.Printf("The length of buffer: %d\n", buffer2.Len())
+	fmt.Printf("The capacity of buffer: %d\n", buffer2.Cap())
+	fmt.Print("\n\n")
 }
