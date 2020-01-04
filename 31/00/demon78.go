7d4992d05261f9ebd701896428e81da4ddb75d5f
yann <tmp4321@qq.com>
2020-01-04 18:41:13 +0800

    buffer1.Read()

--- 31/00/demon78.go
+++ 31/00/demon78.go
@@ -10,7 +10,14 @@ func main() {
 	contents := "Simple byte buffer for marshaling data."
 	fmt.Printf("Write contents %q ...\n", contents)
 	buffer1.WriteString(contents)
+	
 	fmt.Printf("The length of buffer: %d\n", buffer1.Len())
 	fmt.Printf("The capacity of buffer: %d\n", buffer1.Cap())
 	fmt.Println("")
+
+	p1 := make([]byte, 7)
+	n, _ := buffer1.Read(p1)
+	fmt.Printf("%d bytes were read. (call Read)\n", n)
+	fmt.Printf("The length of buffer: %d\n", buffer1.Len())
+	fmt.Printf("The capacity of buffer: %d\n", buffer1.Cap())
 }
