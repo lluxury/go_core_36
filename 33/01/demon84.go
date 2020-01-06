b442af4ca9aeb09231d1b8904466ca831a6a74e0
yann <tmp4321@qq.com>
2020-01-06 23:27:38 +0800

    *Reader.Peek()
    Signed-off-by: yann <tmp4321@qq.com>

--- 33/01/demon84.go
+++ 33/01/demon84.go
@@ -20,25 +20,41 @@ func main() {
 	reader1 := bufio.NewReader(basicReader)
 	//fmt.Printf("The default size of buffered reader: %d\n",reader1.Size())
 	//// 缓冲未填充
-	fmt.Printf("The number of unread bytes in the buffer: %d\n",reader1.Buffered())
-	//fmt.Println("",)
-	
-	bytes, err := reader1.Peek(7)
+	//fmt.Printf("The number of unread bytes in the buffer: %d\n",reader1.Buffered())
+	////fmt.Println("",)
+	//
+	//bytes, err := reader1.Peek(7)
+	//if err != nil {
+	//	fmt.Printf("error: %v\n",err)
+	//}
+	//fmt.Printf("Peeked contents(%d): %q\n",len(bytes),bytes)
+	//fmt.Printf("The number of unread bytes in the buffer: %d\n",reader1.Buffered())
+	//fmt.Println("",)   // 缓冲填满
+	//
+	//buf1 := make([]byte,7)
+	//n, err := reader1.Read(buf1)
+	//if err != nil {
+	//	fmt.Printf("error: %v\n",err)
+	//}
+	//fmt.Printf("Read contents(%d): %q\n",n,buf1)
+	//fmt.Printf("The number of unread bytes in the buffer: %d\n",reader1.Buffered())
+	//fmt.Println("",)  // reader1 是 bufio
+
+	fmt.Printf("Reset the basic reader (size: %d) ...\n", len(comment))
+	basicReader.Reset(comment)
+	fmt.Printf("Reset The buffered reader (size: %d) ...\n",reader1.Size())
+	reader1.Reset(basicReader)
+	peekNum := len(comment) + 1
+	fmt.Printf("Peek %d bytes ...\n",peekNum)
+	bytes, err := reader1.Peek(peekNum)
 	if err != nil {
 		fmt.Printf("error: %v\n",err)
 	}
-	fmt.Printf("Peeked contents(%d): %q\n",len(bytes),bytes)
-	fmt.Printf("The number of unread bytes in the buffer: %d\n",reader1.Buffered())
-	fmt.Println("",)   // 缓冲填满
+	fmt.Printf("The number of peeked bytes: %d\n",len(bytes))
+	fmt.Println("",)
 	
-	buf1 := make([]byte,7)
-	n, err := reader1.Read(buf1)
-	if err != nil {
-		fmt.Printf("error: %v\n",err)
-	}
-	fmt.Printf("Read contents(%d): %q\n",n,buf1)
-	fmt.Printf("The number of unread bytes in the buffer: %d\n",reader1.Buffered())
-	fmt.Println("",)  // reader1 是 bufio 
+	
+	
 
 	
 	
