98333360dd184442a9d426746924e5db7a94daf3
yann <tmp4321@qq.com>
2020-01-06 13:35:49 +0800

    strings.Reader
    Signed-off-by: yann <tmp4321@qq.com>

--- 32/00/demon81.go
+++ 32/00/demon81.go
@@ -11,5 +11,15 @@ func main() {
 	_ = interface{}(builder).(io.Writer)
 	_ = interface{}(builder).(io.ByteWriter)
 	_ = interface{}(builder).(fmt.Stringer)
+
+	reader := strings.NewReader("")
+	_ = interface{}(reader).(io.Reader)
+	_ = interface{}(reader).(io.ReaderAt)
+	_ = interface{}(reader).(io.ByteReader)
+	_ = interface{}(reader).(io.RuneReader)
+	_ = interface{}(reader).(io.Seeker)
+	_ = interface{}(reader).(io.ByteScanner)
+	_ = interface{}(reader).(io.RuneScanner)
+	_ = interface{}(reader).(io.WriterTo)
 }
 
