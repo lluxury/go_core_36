d8c5b99466395eb55aa103e9dd0a65964e164ca5
yann <tmp4321@qq.com>
2020-01-06 15:21:20 +0800

    *io.LimitedReader
    Signed-off-by: yann <tmp4321@qq.com>

--- 32/01/demon82.go
+++ 32/01/demon82.go
@@ -13,27 +13,41 @@ func main() {
 		"into shared public interfaces that abstract the functionality, " +
 		"plus some other related primitives."
 
-	fmt.Println("New a string reader and name it \"reader1\" ...")
+	//fmt.Println("New a string reader and name it \"reader1\" ...")
 	reader1 := strings.NewReader(comment)
 	buf1 := make([]byte, 7)
 	n, err := reader1.Read(buf1)
-	var offset1, index1 int64
-	executeIfNoErr(err, func() {
-		//fmt.Printf("Read(%d): %q\n", buf1[:n])
-		fmt.Printf("Read(%d): %q\n", n, buf1[:n])
-		offset1 = int64(53)
-		index1, err = reader1.Seek(offset1, io.SeekCurrent)
-	})
-	executeIfNoErr(err, func() {
-		fmt.Printf("The new index after seeking from current with offset %d: %d\n",
-			offset1, index1)
-		n, err = reader1.Read(buf1)
-	})
-	executeIfNoErr(err, func() {
-		fmt.Printf("Read(%d): %q\n", n, buf1[:n])
-		fmt.Println("", )
+	//var offset1, index1 int64
+	//executeIfNoErr(err, func() {
+	//	//fmt.Printf("Read(%d): %q\n", buf1[:n])
+	//	fmt.Printf("Read(%d): %q\n", n, buf1[:n])
+	//	offset1 = int64(53)
+	//	index1, err = reader1.Seek(offset1, io.SeekCurrent)
+	//})
+	//executeIfNoErr(err, func() {
+	//	fmt.Printf("The new index after seeking from current with offset %d: %d\n",
+	//		offset1, index1)
+	//	n, err = reader1.Read(buf1)
+	//})
+	//executeIfNoErr(err, func() {
+	//	fmt.Printf("Read(%d): %q\n", n, buf1[:n])
+	//	fmt.Println("", )
+	//
+	//})
 
-	})
+	reader1.Reset(comment)
+	num1 := int64(7)
+	fmt.Printf("New a limited reader with reader1 and number %d ...\n", num1)
+	reader2 := io.LimitReader(reader1,7)
+	buf2 := make([]byte,10)
+	for i := 0; i < 3; i++ {
+		n, err = reader2.Read(buf2)
+		executeIfNoErr(err, func() {
+			fmt.Printf("Read(%d): %q\n",n, buf2[:n])
+		})
+		fmt.Println("",)
+	}
+	
 
 }
 func executeIfNoErr(err error, f func()) {
