26071c270e25e50db035acb39bd82b8beac270ea
yann <tmp4321@qq.com>
2020-01-06 13:46:44 +0800

    CopyN
    Signed-off-by: yann <tmp4321@qq.com>

--- 32/00/demon81.go
+++ 32/00/demon81.go
@@ -36,8 +36,19 @@ func main() {
 	_ = interface{}(buffer).(io.ReaderFrom)
 
 	_ = interface{}(buffer).(fmt.Stringer)
-	bytes.Buffer{}
 
+	src := strings.NewReader(
+		"CopyN copies n bytes (or until an error) from src to dst. " +
+			"It returns the number of bytes copied and " +
+			"the earliest error encountered while copying.")
+	dst := new(strings.Builder)
+	written, err := io.CopyN(dst, src, 58)
+	if err != nil {
+		fmt.Printf("error: %v\n",err)
+	} else {
+		fmt.Printf("Written(%d): %q\n",written, dst.String())
+
+	}
 
 }
 
