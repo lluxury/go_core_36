ade4cae9916f27020a6e2531989088605efe94a9
yann <tmp4321@qq.com>
2020-01-07 10:48:33 +0800

    Read method
    Signed-off-by: yann <tmp4321@qq.com>

--- 33/02/demon85.go
+++ 33/02/demon85.go
@@ -16,24 +16,34 @@ func main() {
 	fmt.Printf("The size of basic reader: %d\n",basicReader.Size())
 
 	size := 300
-	fmt.Printf("New a buffered reader with size %d ...\n",size)
+	//fmt.Printf("New a buffered reader with size %d ...\n",size)
 	reader1 := bufio.NewReaderSize(basicReader,size)
-	fmt.Println("",)
+	//fmt.Println("",)
+	//
+	//fmt.Print("[ About 'Peek' method]\n\n")
+	//peekNum := 38
+	//fmt.Printf("Peek %d bytes ...\n",peekNum)
+	//bytes , err := reader1.Peek(peekNum)
+	//if err != nil {
+	//	fmt.Printf("error: %v\n",err)
+	//}
+	//fmt.Printf("Peeked contents(%d): %q\n", len(bytes),bytes)
+	//fmt.Printf("The number of unread bytes in the buffer: %d\n",reader1.Buffered())
+	//fmt.Println("",)
+	//
+	fmt.Print("[ About 'Read' method ]\n\n")
 
-	fmt.Print("[ About 'Peek' method]\n\n")
-	peekNum := 38
-	fmt.Printf("Peek %d bytes ...\n",peekNum)
-	bytes , err := reader1.Peek(peekNum)
+	readNum := 38
+	buf1 := make([]byte,readNum)  // 准备切片
+	fmt.Printf("Read %d bytes ...\n",readNum) // 读数
+	n, err := reader1.Read(buf1)
 	if err != nil {
-		fmt.Printf("error: %v\n",err)
+		fmt.Printf("error: %\n", err)
 	}
-	fmt.Printf("Peeked contents(%d): %q\n", len(bytes),bytes)
-	fmt.Printf("The number of unread bytes in the buffer: %d\n",reader1.Buffered())
+	fmt.Printf("Read contents(%d): %q\n",n,buf1)  // 内容
+	fmt.Printf("The number of unread bytes in the buffer : %d\n",reader1.Buffered()) // 读计数
 	fmt.Println("",)
-
-	//fmt.Print("[ About 'Read' method ]\n\n")
-
-
+	//fmt.Print("[ About 'ReadSlice' method ]\n\n")
 	
 	
 	
