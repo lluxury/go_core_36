c01ed90b72373134244f278e8a34e975a13516bc
yann <tmp4321@qq.com>
2020-01-08 16:51:01 +0800

    *ServeMux
    Signed-off-by: yann <tmp4321@qq.com>

--- 36/02/demon95.go
+++ 36/02/demon95.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"sync"
@@ -13,6 +14,8 @@ func main() {
 		wg.Add(2)
 
 		go startServer1(&wg)
+		go startServer2(&wg)
+
 
 		wg.Wait()
 
@@ -30,3 +33,34 @@ func startServer1(wg *sync.WaitGroup)  {
 		}
 	}
 }
+
+func startServer2(wg *sync.WaitGroup) {
+	defer wg.Done()
+	mux1 := http.NewServeMux()
+	mux1.HandleFunc("/hi", func(w http.ResponseWriter, req *http.Request) {
+		//if req.URL.Path != "hi"{
+		if req.URL.Path != "/hi"{
+			http.NotFound(w, req)
+			return
+		}
+		name := req.FormValue("name")
+		if  name == "" {
+			fmt.Fprint(w, "Welcome!")
+		} else {
+			fmt.Fprintf(w, "Welcome, %s!", name)
+		}
+	})
+	httpServer2 := http.Server{
+		Addr:              "127.0.0.1:8081",
+		Handler:           mux1,
+	}
+	if err := httpServer2.ListenAndServe();err != nil{
+		if err == http.ErrServerClosed{
+			log.Println("HTTP server 2 closed.")
+		} else {
+			log.Printf("HTTP server 2 error: %v\n", err)
+		}
+	}
+}
+
+//curl -X POST 127.0.0.1:8081/hi -d "name=xx"
